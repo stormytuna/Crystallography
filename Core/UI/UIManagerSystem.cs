de824b9273e98925ef809015893dbd539db0174c
Roton <140523534+RotonDev@users.noreply.github.com>
2025-06-24 03:38:10 +0200

    extra null checks

--- Core/UI/UIManagerSystem.cs
+++ Core/UI/UIManagerSystem.cs
@@ -54,10 +54,10 @@ public class UIManagerSystem : ModSystem {
 	public override void UpdateUI(GameTime gameTime) {
 		GemSlotsMenu?.Update(gameTime);
 		GemballMachineMenu?.Update(gameTime);
-		if (GemballInterface != null) {
+		if (GemballInterface != null && GemballInterface.TilePosition != null && GemballInterface.slot != null && GemballInterface.slot.Slot != null) {
 			if (GemballInterface.TilePosition.ToWorldCoordinates().Distance(Main.LocalPlayer.Center) > 80 && GemballInterface.slot.Slot != null && !GemballInterface.slot.Slot.IsAir) {
 				GemballMachineMenu.SetState(null);
-				var item = GemballInterface.slot.Slot.Clone();
+				var item = GemballInterface?.slot?.Slot.Clone();
 				Item.NewItem(item.GetSource_DropAsItem(), GemballInterface.TilePosition.ToWorldCoordinates(), item);
 				GemballInterface.slot.Slot.TurnToAir();
 			}
